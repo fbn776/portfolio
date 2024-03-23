48f5da05ccc252495d881d05d5896cac2f751390
fbn776 <fbn776@gmail.com>
2024-03-24 01:02:26 +0530

    feat: education time line

--- src/app/data/EducationData.ts
+++ src/app/data/EducationData.ts
@@ -1,31 +1,35 @@
 
 interface EducationModel {
-	from: string,
-	to: string,
+	title: string,
 	institution: string,
 	course: string,
+	description?: string,
+	marks?: string
 }
 
 const EducationData: EducationModel[] = [
 	{
-		from: "2019",
-		to: "2020",
+		title: '2019 - 2021',
 		institution: "St Aloysius Senior Secondary School",
+		description: "In my 10th grade, I attended St. Aloysius Senior Secondary School, a CBSE-affiliated institution, where I attained a commendable score of 90% in the final examination. This achievement underscores both my commitment to academic excellence and the high standard of education offered by the school.",
 		course: "10th standard",
+		marks: '90%'
 	},
 
 	{
-		from: "2021",
-		to: "2022",
+		title: '2021 - 2022',
 		institution: "St Aloysius Senior Secondary School",
+		description: "In my 12th standard at St. Aloysius Senior Secondary School, I attained a notable score of 93.4% in the final exam, reflecting my commitment to academic excellence within a reputable CBSE-affiliated institution.",
 		course: "12th standard",
+		marks: '93.4%',
 	},
 
 	{
-		from: "2022",
-		to: "2026",
+		title: '2022 - 2026',
 		institution: "Rajiv Gandhi Institute of Technology Kottayam",
+		description: "At Rajiv Gandhi Institute of Technology (RIT), one of Kerala's most prestigious Government Engineering Colleges (GEC), I'm currently pursing my Bachelor of Technology in Computer Science and Engineering. And currently has Cumulative Grade Point Average (CGPA) of 9.27.",
 		course: "BTech in CSE",
+		marks: '9.27 cpga'
 	},
 ];
 
--- src/app/ui/about/EducationTimeline.tsx
+++ src/app/ui/about/EducationTimeline.tsx
@@ -1,21 +1,40 @@
 "use client";
 
-import { IconSchool } from '@tabler/icons-react';
-import Timeline from "rsuite/Timeline";
-import './style.css'
-import EducationData from '@/app/data/EducationData';
+import EducationData from "@/app/data/EducationData";
+import "./style.css";
+
 
 export default function EducationTimeline() {
 	return (
-		<div className="w-full text-white">
-			<Timeline>
-				{EducationData.map((data, i) => {
-					return <Timeline.Item key={i} time={`${data.from} - ${data.to}`}>
-						<h1>{data.institution}</h1>
-						<h2>{data.course}</h2>
-					</Timeline.Item>;
-				})}
-			</Timeline>
+		<div className="timeline">
+			{
+				EducationData.map((data, i) => {
+					return (
+						<div key={i} className="container" data-title={data.title}>
+							<div className="content">
+								<h1 className="text-xl mb-2 font-semibold">
+									{data.institution}
+								</h1>
+								<li> {data.course}</li>
+								{data.description ? (
+									<p className="text-pretty m-2 text-base font-light opacity-70">
+										{data.description}
+									</p>
+								) : (
+									""
+								)}
+								{data.marks ? (
+									<li>
+										{data.marks}
+									</li>
+								) : (
+									""
+								)}
+							</div>
+						</div>
+					);
+				})
+			}
 		</div>
 	);
 }
