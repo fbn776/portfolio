74fadfd9d6a4cb6d737616205abb8c27676ad024
fbn776 <fbn776@gmail.com>
2024-04-09 01:24:40 +0530

    Update project data

--- src/app/data/projects/FeaturedProjectsData.ts
+++ src/app/data/projects/FeaturedProjectsData.ts
@@ -14,7 +14,7 @@ const ProjectsData: IProjects[] = [
 		description:
 			"UniMate is your go-to companion for navigating college life smoothly and effortlessly. Designed with college students in mind, Unimate simplifies your daily routines, helps you stay organized, and empowers you to excel in your academic journey. UniMate was built by my team HackHive during a 8 hour Hackthon conducted at RIT Kottayam.",
 		screenShotSrc: "/projects/unimate.webp",
-		technologies: ["React", "Node.js", "Express", "MongoDB"],
+		technologies: ["NextJS", "ExpressJS", "GenAI", "Education"],
 		github: "https://github.com/fbn776/UniMate",
 	},
 	{
@@ -22,7 +22,7 @@ const ProjectsData: IProjects[] = [
 		description:
 			"The AssistScript Chatbot Assistant is an innovative web application designed to provide users with intuitive and efficient assistance using the specially crafted AssistScript language. As a versatile virtual assistant, it offers a wide range of functionalities tailored to meet users' needs, from retrieving information to performing tasks and facilitating communication.",
 		screenShotSrc: "/projects/assistant.webp",
-		technologies: ["React", "Node.js", "Express", "MongoDB"],
+		technologies: ["NextJS", "TS", "Interpreter", "Chat bot"],
 		github: "https://github.com/fbn776/Assistant",
 		website: "https://assistant-ebon.vercel.app/",
 	},
