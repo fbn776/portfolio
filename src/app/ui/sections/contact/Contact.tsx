21a43cb4c623bc47f0fd54088f5c2f584808473b
fbn776 <fbn776@gmail.com>
2024-04-09 20:36:09 +0530

    Refactor

--- src/app/ui/sections/contact/Contact.tsx
+++ src/app/ui/sections/contact/Contact.tsx
@@ -1,24 +1,6 @@
-import "./style.css"
+import "./style.css";
 import Header from "../../components/Header";
-import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandX, IconCaretRight, IconError404 } from "@tabler/icons-react";
-import Link from "next/link";
-import { SocialLinks } from "@/app/data/Links";
-
-const getBrandIcon = (name: string) => {
-	switch(name) {
-		case "instagram":
-			return <IconBrandInstagram size={40} />
-		case "github":
-			return <IconBrandGithub size={40} />
-		case "linkedin":
-			return <IconBrandLinkedin size={40} />;
-		case "twitter":
-			return <IconBrandX size={40} />
-		default:
-			return <IconError404 size={40} />;
-	}
-}
-
+import Socials from "./Socials";
 
 export default function Contact() {
 	return (
@@ -34,30 +16,7 @@ export default function Contact() {
 			<div className="w-full flex justify-stretch mt-10 max-lg:flex-col">
 				<div className="w-full h-full border-sep ">
 					<h2 className="text-center text-4xl">Socials</h2>
-					<div >
-						{SocialLinks.map((item, index) => {
-							return (
-								<Link
-									key={index}
-									href={item.link}
-									className="w-[80%] m-auto flex p-4 items-center gap-5 my-5 bg-red-200 glass-bg rounded-lg shadow-lg"
-								>
-									<div className="size-[60px] bg-primary rounded-full flex items-center justify-center">
-										{getBrandIcon(item.name)}
-									</div>
-									<div className="flex flex-col gap-1">
-										<label className="capitalize tracking-wide">
-											{item.name}
-										</label>
-										<span className="text-xs opacity-90 flex items-center">
-											@{item.username}
-										</span>
-										<span className="text-xs opacity-60 flex">{item.link}</span>
-									</div>
-								</Link>
-							);
-						})}
-					</div>
+					<Socials />
 				</div>
 				<div className="w-full h-full">
 					<h2 className="text-center text-4xl">Connect</h2>
---
+++ src/app/ui/sections/contact/Socials.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { SocialLinks } from "../../../data/Links";
+import {
+	IconBrandGithub,
+	IconBrandInstagram,
+	IconBrandLinkedin,
+	IconBrandX,
+	IconError404,
+	IconExternalLink,
+} from "@tabler/icons-react";
+
+const getBrandIcon = (name: string) => {
+	switch (name) {
+		case "instagram":
+			return <IconBrandInstagram className="size-full" />;
+		case "github":
+			return <IconBrandGithub className="size-full" />;
+		case "linkedin":
+			return <IconBrandLinkedin className="size-full" />;
+		case "twitter":
+			return <IconBrandX className="size-full" />;
+		default:
+			return <IconError404 className="size-full" />;
+	}
+};
+
+export default function Socials() {
+	return SocialLinks.map((item, index) => {
+		return (
+			<Link
+				key={index}
+				href={item.link}
+				className="w-[80%] max-sm:w-full m-auto flex p-4 justify-between items-center gap-5 max-lg:gap-3 my-5 bg-red-200 glass-bg rounded-lg shadow-lg"
+			>
+				<div className="size-[60px] p-2 lg:p-3 aspect-square max-sm:size-[40px] bg-primary rounded-full flex items-center justify-center">
+					{getBrandIcon(item.name)}
+				</div>
+				<div className="w-full">
+					<label className="capitalize tracking-wide">{item.name}</label>
+					<span className="text-xs opacity-60 flex items-center">
+						@{item.username}
+					</span>
+				</div>
+				<IconExternalLink size={40}/>
+			</Link>
+		);
+	});
+}
