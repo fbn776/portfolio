150e583d7d3057c5484202299413608a446819bb
fbn776 <fbn776@gmail.com>
2024-04-13 21:20:53 +0530

    add resumee

---
+++ src/app/data/Resume.ts
@@ -0,0 +1,7 @@
+const Resume = {
+	driveLink:
+		"https://drive.google.com/file/d/1NhoUbt2D1o__pBHqbW2tcmHxb00XH6kJ/view?usp=drive_link",
+	fileLink: "/public/files/Febin Nelson P - resume.pdf",
+};
+
+export default Resume;
