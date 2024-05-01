dc22ad11ceca0207c74f18f2bd2c99a6c5396c53
fbn776 <fbn776@gmail.com>
2024-05-01 11:24:51 +0530

    feat: add a time delay in which navbar stays open

--- src/app/lib/useScrollEffect.ts
+++ src/app/lib/useScrollEffect.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import {useEffect, useRef} from "react";
 
 /**
  * Hook to control the navbar scroll effect, ie when scrolling down it stays hidden, when scrolling up it shows up.
@@ -6,31 +6,39 @@ import { useEffect, useRef } from "react";
  * @returns a ref to the navbar element
  */
 export default function useScrollEffect(parentSelector: string) {
-	const navbar = useRef<HTMLElement>(null);
+    const navbar = useRef<HTMLElement>(null);
+    let lastVisibleTime = 0;
 
-	useEffect(() => {
-		const parent = document.querySelector(parentSelector);
-		let lastScroll = 0;
+    useEffect(() => {
+        const parent = document.querySelector(parentSelector);
+        let lastScroll = 0;
 
-		function scrollFunc() {
-			if (!navbar.current) return;
-			const currScroll = parent?.scrollTop || 0;
+        function scrollFunc() {
+            if (!navbar.current) return;
+            const currScroll = parent?.scrollTop || 0;
 
-			navbar.current.classList.toggle("scrolled-state", currScroll > 50);
+            navbar.current.classList.toggle("scrolled-state", currScroll > 50);
 
-			if (currScroll > 200)
-				navbar.current.classList.toggle("nav-hide", currScroll > lastScroll);
+            if (currScroll > 200) {
+                const hideNav = currScroll > lastScroll;
 
-			lastScroll = currScroll;
-		}
+                if (Date.now() - lastVisibleTime > 1000 || !hideNav) {
+                    navbar.current.classList.toggle("nav-hide", hideNav);
 
-		scrollFunc();
-		parent?.addEventListener("scroll", scrollFunc);
+                    lastVisibleTime = Date.now();
+                }
+            }
 
-		return () => {
-			parent?.removeEventListener("scroll", scrollFunc);
-		};
-	}, [parentSelector]);
+            lastScroll = currScroll;
+        }
 
-	return [navbar];
+        scrollFunc();
+        parent?.addEventListener("scroll", scrollFunc);
+
+        return () => {
+            parent?.removeEventListener("scroll", scrollFunc);
+        };
+    }, [parentSelector]);
+
+    return [navbar];
 }
