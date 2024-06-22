284f7698461d2fecffb6513b2e00776327823fea
fbn776 <fbn776@gmail.com>
2024-06-22 12:07:46 +0530

    chore: add updated resume

--- src/app/data/Resume.ts
+++ src/app/data/Resume.ts
@@ -1,10 +1,11 @@
 const Resume = {
 	driveLink:
-		"https://drive.google.com/file/d/1_bFFxFL4sdLNEwGHrvuofyP_vqVckZqY/view?usp=sharing",
+		"https://drive.google.com/file/d/19YVlOyvka2HuxV_WwB9QvKH0WeN3df_4/view?usp=drive_link",
 
 	//https://drive.google.com/file/d/1_bFFxFL4sdLNEwGHrvuofyP_vqVckZqY/view?usp=sharing
 	// ^ direct link to the file
-	fileLink: "files/Febin Nelson P - resume.pdf",
+
+	fileLink: "files/Febin_Nelson_22-06-24.pdf",
 };
 
 export default Resume;
